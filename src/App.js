75f96b2c9a7c71513f23b6c938e29832bc766860
avinash gupta <gavinash6290@gmail.com>
2022-10-25 22:50:16 +0530

    profile card done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 // Components
 import Navigator from "./components/Navigator";
 import Sidebar from "./components/Sidebar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className="App">
       <Navigator />
-      <div className="container bd">
-        <div className="prof-graph-container">
-
+      <div className="container">
+        <div className="prof-graph-container bd">
+          <div className="top">
+            <Profile name="Talan Phillips" age={24} bloodGroup={"A+"} height={"187cm"} weight={"91Kgs"} />
+          </div>
           <div className="graph-container"></div>
         </div>
         <Sidebar />
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,32 @@
 import "./index.css";
 
+import man from "../../assets/man.jpg";
+
 const Profile = ({ name, age, bloodGroup, height, weight }) => {
     return (
         <div className="profile-card">
-
+            <img src={man} className={"photo"} />
+            <h4>{name}</h4>
+            <div className="info-container">
+                <div className="info br bb">
+                    <h4>{age}</h4>
+                    <p>Years</p>
+                </div>
+                <div className="info bl bb">
+                    <h4>{bloodGroup}</h4>
+                    <p>Blood</p>
+                </div>
+            </div>
+            <div className="info-container">
+                <div className="info br bt">
+                    <h4>{height}</h4>
+                    <p>Height</p>
+                </div>
+                <div className="info bl bt">
+                    <h4>{weight}</h4>
+                    <p>Weight</p>
+                </div>
+            </div>
         </div>
     );
 }
